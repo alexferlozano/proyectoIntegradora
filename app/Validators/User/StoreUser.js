2e7a671b1bd792cf2510faa4b02ed3f4e3a63435
Alex Lozano <19170025@uttcampus.edu.mx>
2021-03-31 10:45:16 -0600

    [UPD] Correccion de validator

--- app/Validators/User/StoreUser.js
+++ app/Validators/User/StoreUser.js
@@ -11,9 +11,6 @@ class StoreUser {
 
   get rules () {
     return {
-      first_name: 'required|max:100',
-      last_name: 'required|max:100',
-      second_last_name: 'required|max:100',
       email: 'required|email|unique:users|max:254',
       password: 'required|max:60',
       username: 'required|max:80'
