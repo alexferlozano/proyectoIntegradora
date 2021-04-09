42a99f54b5ec5d4c0b2920ed082a9cf123af909d
Alex Lozano <19170025@uttcampus.edu.mx>
2021-04-09 12:51:34 -0500

    [UPD] fixed update

--- start/routes/user.js
+++ start/routes/user.js
@@ -4,6 +4,8 @@ const Route = use('Route')
 Route.group(() => {
   Route.get('', 'UserController.getProfile')
   Route.put('edit', 'UserController.editProfile')
+  Route.put('edit/email', 'UserController.editEmail').validator('User/UpdateUserEmail')
+  Route.put('edit/username', 'UserController.editUsername').validator('User/UpdateUser')
 })
   .prefix('v1/api/profile/')
   .middleware(['auth:jwt'])
